abdec387bb9d966cf606923430ec401b5018a098
ken <396989681@qq.com>
2020-08-25 23:06:25 +0800

    1

--- src/Count.tsx
+++ src/Count.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 interface ICountProps {
     initial: number,
@@ -10,16 +10,34 @@ interface IArticleProps {
 }
 
 const Count: React.FC<ICountProps> = ({ initial = 0 }) => {
+
     const [count, setCount] = useState<number>(initial);
     const [article, setArticle] = useState<IArticleProps>({ title: 'title', content: 'content' })
 
+    const countRef = useRef<number>(count);
+
+
+
     useEffect(() => {
-        console.log(count);
+        // console.log(count);
+        setArticle({
+            title: `当前是第${count}篇标题`,
+            content: `当前是第${count}篇内容`
+        })
+        countRef.current = count
     }, [count])
 
+    const handleCount = () => {
+        setTimeout(() => {
+            // alert('current count: ' + count)
+            alert('current count: ' + countRef.current)
+        }, 3000);
+    }
+
     const handleArticle = (): void => {
-        console.log(article.title);
-        console.log(count);
+        setCount(count + 1);
+        // 这里监听的count还是之前的count 
+        // 因此 如果在这里setArticle 则count有差异
     }
 
     return (
@@ -27,6 +45,7 @@ const Count: React.FC<ICountProps> = ({ initial = 0 }) => {
             <p>Count: {count}</p>
             <button onClick={() => setCount(count + 1)}>加</button>
             <button onClick={() => setCount(count - 1)}>减</button>
+            <button onClick={() => handleCount()}>弹框显示</button>
             <p>Title: {article.title}</p>
             <section>Content:{article.content}</section>
             <button onClickCapture={() => handleArticle()}>
