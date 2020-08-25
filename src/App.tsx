902aee33bfd2842421fbd1a1b0a72c430d3043fb
ken <396989681@qq.com>
2020-08-25 22:06:06 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import User from './User';
+import Count from './Count';
 function App() {
   return (
     <div className="App">
       <User name='vortesnail' age={25}>这里是children</User>
+      <Count initial={0}></Count>
     </div>
   );
 }
--- src/Count.tsx
+++ src/Count.tsx
@@ -1,16 +1,40 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface ICountProps {
-    int: number
+    initial: number,
 }
 
-const Count: React.FC<ICountProps> = (int) => {
+interface IArticleProps {
+    title: string,
+    content: string,
+}
+
+const Count: React.FC<ICountProps> = ({ initial = 0 }) => {
+    const [count, setCount] = useState<number>(initial);
+    const [article, setArticle] = useState<IArticleProps>({ title: 'title', content: 'content' })
+
+    useEffect(() => {
+        console.log(count);
+    }, [count])
+
+    const handleArticle = (): void => {
+        console.log(article.title);
+        console.log(count);
+    }
+
     return (
         <div>
-
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>加</button>
+            <button onClick={() => setCount(count - 1)}>减</button>
+            <p>Title: {article.title}</p>
+            <section>Content:{article.content}</section>
+            <button onClickCapture={() => handleArticle()}>
+                下一篇
+            </button>
         </div>
     );
- 
+
 };
 
 export default Count;
